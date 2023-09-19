fec1d7cb47275433c46c00136c456e84f772d371
Yeaeun411 <yeaeun411@naver.com>
2023-09-19 14:32:03 +0900

    fix err

--- JavaProgramming/study/Study/Bronze5_1330.java
+++ JavaProgramming/study/Study/Bronze5_1330.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 
 public class Bronze5_1330 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        int A = scanner.nextInt();
-        int B = scanner.nextInt();
-        
-        if (A > B) {
-            System.out.println(">");
-        } else if (A < B) {
-            System.out.println("<");
-        } else {
-            System.out.println("==");
+        try (Scanner scanner = new Scanner(System.in)) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            
+            if (A > B) {
+                System.out.println(">");
+            } else if (A < B) {
+                System.out.println("<");
+            } else {
+                System.out.println("==");
+            }
         }
     }
 }
